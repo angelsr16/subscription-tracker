4d182ea85058a0001ab5a53ff1a82482ee874621
Ubuntu <ubuntu@ip-172-31-28-0.ec2.internal>
2025-10-27 23:26:26 +0000

    server changes

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: ["https://subscription-tracker-ashy.vercel.app","http://localhost:3000"],
         allowedHeaders: ["Authorization", "Content-Type"],
         credentials: true,
     })
@@ -30,7 +30,7 @@ app.use(errorMiddleware);
 
 app.get("/", (req, res) => { res.send("Welcome to the Subscription Tracker API") });
 
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT, '127.0.0.1', async () => {
     console.log(`Subscription Tracker API is running on http://localhost:${process.env.PORT}`);
 
     await connectToDatabase();
